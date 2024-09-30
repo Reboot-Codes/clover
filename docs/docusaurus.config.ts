f87e43a2dd6c6fe0aab4f13ff1fd864ea94090f2
Reboot-Codes <git@reboot-codes.com>
2024-09-30 13:06:15 -0700

    Fix root domain

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.reboot-codes.com',
+  url: 'https://clover.reboot-codes.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: `/`,
