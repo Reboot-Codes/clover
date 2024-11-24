2f485bea763b7cd77c9db734113520416a82e223
Reboot-Codes <git@reboot-codes.com>
2024-11-23 22:57:24 -0700

    FUCK YEAHHHHHH

--- shell.nix
+++ shell.nix
@@ -1,49 +1,92 @@
-{ pkgs ? import <nixpkgs> {} }:
-  let
-    overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
-    libPath = with pkgs; lib.makeLibraryPath [
-      # load external libraries that you need in your rust project here
-    ];
-in
-  pkgs.mkShell rec {
-    buildInputs = with pkgs; [
-      clang
-      # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
-      llvmPackages.bintools
-      rustup
-      nodejs_22
-      yarn-berry
-      xorg.libX11
-      openssl
-      pkg-config
-    ];
-    RUSTC_VERSION = overrides.toolchain.channel;
-    # https://github.com/rust-lang/rust-bindgen#environment-variables
-    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-    shellHook = ''
-      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-      '';
-    # Add precompiled library to rustc search path
-    RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-      # add libraries here (e.g. pkgs.libvmi)
-    ]);
-    LD_LIBRARY_PATH = libPath;
-    # Add glibc, clang, glib, and other headers to bindgen search path
-    BINDGEN_EXTRA_CLANG_ARGS =
-    # Includes normal include path
-    (builtins.map (a: ''-I"${a}/include"'') [
-      # add dev libraries here (e.g. pkgs.libvmi.dev)
-      pkgs.glibc.dev
-      pkgs.xorg.libX11.dev
-      pkgs.openssl.dev
-    ])
-    # Includes with special directory paths
-    ++ [
-      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-      ''-I${pkgs.xorg.libX11.out}/lib/''
-    ];
-  }
+{ pkgs ? import <nixpkgs> {} }: /*
+based on
+https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
+*/
+let
+# Toolchain.
+gccVersion = "13";
+gccPkg = pkgs."gcc${gccVersion}";
+llvmVersion = "17";
+llvmPkgs = pkgs."llvmPackages_${llvmVersion}";
+clangTools = pkgs."clang-tools_${llvmVersion}";
+clanggccPkg = pkgs.gccPkgAdapters.overrideCC llvmPkgs.gccPkg (llvmPkgs.clang.override {gccForLibs = gccPkg;});
+libTriple = "x86_64-unknown-linux-gnu";
 
+compilerLinks = pkgs.runCommand "clang-links" {} ''
+  mkdir -p $out/bin
+  mkdir -p $out/usr/${libTriple}/usr/lib/
+  ln -s ${llvmPkgs.clang}/bin/clang $out/bin/clang-${llvmVersion}
+  ln -s ${llvmPkgs.clang}/bin/clang++ $out/bin/clang++-${llvmVersion}
+  ln -s ${llvmPkgs.llvm}/bin/llvm-as $out/bin/llvm-as-${llvmVersion}
+  ln -s ${gccPkg.cc}/lib/gcc/${libTriple}/${gccPkg.cc.version}/crtbeginS.o $out/usr/${libTriple}/usr/lib/crtbeginS.o
+  ln -s ${gccPkg.cc}/lib/gcc/${libTriple}/${gccPkg.cc.version}/crtendS.o $out/usr/${libTriple}/usr/lib/crtendS.o
+'';
+
+dependencies = with pkgs; [
+  # Dependencies
+  fmt
+  protobuf
+  gtk3
+  harfbuzz
+  atk
+  cairo
+  pango
+  gdk-pixbuf
+  webkitgtk_4_1
+  openssl
+];
+
+nativeBuildInputs = with pkgs;
+  [
+    llvmPkgs.bintools-unwrapped
+    lldb_17
+    ninja
+    cmake
+
+    # Toolchain
+
+    # Do not use the clangd from this package as it does not work correctly with
+    # stdlib headers.
+    llvmPkgs.lld
+    llvmPkgs.lldb
+
+    # Compiler Links
+    compilerLinks
+
+    pkg-config
+    gccPkg.cc
+  ]
+  ++ dependencies ++ [ clangTools ];
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  #rustVersion = "1.62.0";
+  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+  };
+
+  hostPlatform = "x86_64-unknown-linux-gnu";
+in pkgs.mkShell {
+  name = "clover-dev";
+
+  buildInputs = [
+    rust
+    gccPkg.cc
+    llvmPkgs.libclang
+  ] ++ (with pkgs; [
+    clang
+  ]);
+
+  shellHook = ''
+    export LIBCLANG_PATH="${llvmPkgs.libclang.lib}/lib";
+    export LD_LIBRARY_PATH="${gccPkg.cc.lib}/lib";
+    export RUST_BACKTRACE=1;
+    # $(< ${gccPkg.cc}/nix-support/libc-crt1-cflags) $(< ${gccPkg.cc}/nix-support/libc-cflags) $(< ${gccPkg.cc}/nix-support/cc-cflags) $(< ${gccPkg.cc}/nix-support/libcxx-cxxflags)
+    export BINDGEN_EXTRA_CLANG_ARGS="-idirafter ${gccPkg.cc}/lib/clang/${pkgs.lib.getVersion gccPkg.cc}/include ${pkgs.lib.optionalString gccPkg.cc.isGNU "-isystem ${gccPkg.cc}/include/c++/${pkgs.lib.getVersion gccPkg.cc} -isystem ${gccPkg.cc}/include/c++/${pkgs.lib.getVersion gccPkg.cc}/${hostPlatform} -idirafter ${gccPkg.cc}/lib/gcc/${hostPlatform}/${pkgs.lib.getVersion gccPkg.cc}/include"}"
+  '';
+
+  inherit nativeBuildInputs;
+}
