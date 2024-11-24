83031014d64dc238dd9226ac16edc882fd493b96
Reboot-Codes <git@reboot-codes.com>
2024-11-24 14:35:46 -0700

    Add wgpu for `renderer`.

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4
 let
 # Toolchain.
 gccVersion = "13";
-gccPkg = pkgs."gcc${gccVersion}";
+gccPkg = pkgs."gcc${gccVersion}"; # pkgs.wrapCCMulti $GCC
 llvmVersion = "17";
 llvmPkgs = pkgs."llvmPackages_${llvmVersion}";
 clangTools = pkgs."clang-tools_${llvmVersion}";
@@ -34,6 +34,21 @@ dependencies = with pkgs; [
   gdk-pixbuf
   webkitgtk_4_1
   openssl
+  wayland
+  libxkbcommon
+  libGL
+  xorg.libX11
+  SDL2
+  SDL2_image
+  egl-wayland
+  eglexternalplatform
+  alsa-lib
+  udev
+  vulkan-loader
+  xorg.libX11
+  xorg.libXrandr
+  xorg.libXcursor
+  xorg.libXi
 ];
 
 nativeBuildInputs = with pkgs;
@@ -82,7 +97,7 @@ in pkgs.mkShell {
 
   shellHook = ''
     export LIBCLANG_PATH="${llvmPkgs.libclang.lib}/lib";
-    export LD_LIBRARY_PATH="${gccPkg.cc.lib}/lib";
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath ([ gccPkg.cc.lib ] ++ dependencies)}";
     export RUST_BACKTRACE=1;
     # $(< ${gccPkg.cc}/nix-support/libc-crt1-cflags) $(< ${gccPkg.cc}/nix-support/libc-cflags) $(< ${gccPkg.cc}/nix-support/cc-cflags) $(< ${gccPkg.cc}/nix-support/libcxx-cxxflags)
     export BINDGEN_EXTRA_CLANG_ARGS="-idirafter ${gccPkg.cc}/lib/clang/${pkgs.lib.getVersion gccPkg.cc}/include ${pkgs.lib.optionalString gccPkg.cc.isGNU "-isystem ${gccPkg.cc}/include/c++/${pkgs.lib.getVersion gccPkg.cc} -isystem ${gccPkg.cc}/include/c++/${pkgs.lib.getVersion gccPkg.cc}/${hostPlatform} -idirafter ${gccPkg.cc}/lib/gcc/${hostPlatform}/${pkgs.lib.getVersion gccPkg.cc}/include"}"
