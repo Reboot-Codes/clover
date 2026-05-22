bb178cb7c4bdba9d17c826b7ab1792ca550b60f6
Reboot-Codes <git@reboot-codes.com>
2026-05-22 16:36:43 -0700

    Add OpenTelemetry to be used later with Tesseract and error reports.

--- clover-hub/src/main.rs
+++ clover-hub/src/main.rs
@@ -11,6 +11,9 @@
 //! Terminal User Interface to manage a clover instance over UART/SSH.
 //!
 
+use opentelemetry_appender_tracing::layer;
+use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_sdk::Resource;
 use std::error::Error;
 use tokio_util::sync::CancellationToken;
 use tracing_log::LogTracer;
@@ -25,13 +28,42 @@ use clover_hub::run;
 pub async fn main() -> Result<(), Box<dyn Error>> {
   LogTracer::init()?;
 
-  let env_filter = EnvFilter::try_from_env("CLOVER_LOG").unwrap_or_else(|_| EnvFilter::new("info"));
-  let stdout_layer = tracing_subscriber::fmt::layer();
+  // let exporter = opentelemetry_stdout::LogExporter::default();
+  let provider: SdkLoggerProvider = SdkLoggerProvider::builder()
+    .with_resource(Resource::builder().with_service_name("clover-hub").build())
+    //  .with_simple_exporter(exporter)
+    .build();
+
+  // To prevent a telemetry-induced-telemetry loop, OpenTelemetry's own internal
+  // logging is properly suppressed. However, logs emitted by external components
+  // (such as reqwest, tonic, etc.) are not suppressed as they do not propagate
+  // OpenTelemetry context. Until this issue is addressed
+  // (https://github.com/open-telemetry/opentelemetry-rust/issues/2877),
+  // filtering like this is the best way to suppress such logs.
+  //
+  // The filter levels are set as follows:
+  // - Allow `info` level and above by default.
+  // - Completely restrict logs from `hyper`, `tonic`, `h2`, and `reqwest`.
+  //
+  // Note: This filtering will also drop logs from these components even when
+  // they are used outside of the OTLP Exporter.
+  let otel_filter = EnvFilter::new("info")
+    .add_directive("hyper=off".parse().unwrap())
+    .add_directive("tonic=off".parse().unwrap())
+    .add_directive("h2=off".parse().unwrap())
+    .add_directive("reqwest=off".parse().unwrap());
+  let otel_layer = layer::OpenTelemetryTracingBridge::new(&provider).with_filter(otel_filter);
+
   // TODO: Send logs to Zenoh log endpoint.
-  // let zenoh_layer = ZenohLogLayer { session: active_session, host_id: "hub-sbc".into() };
+
+  let env_filter = EnvFilter::try_from_env("CLOVER_LOG")
+    .unwrap_or_else(|_| EnvFilter::new("info").add_directive("clover_hub=debug".parse().unwrap()));
+  let stdout_layer = tracing_subscriber::fmt::layer()
+    .with_thread_names(true)
+    .with_filter(env_filter);
 
   let subscriber = tracing_subscriber::registry()
-    .with(env_filter)
+    .with(otel_layer)
     .with(stdout_layer);
   // .with(zenoh_layer);
 
@@ -47,6 +79,8 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     _ = run(big_boy_token.clone()) => {},
     _ = big_boy_token.cancelled() => {}
   }
+
+  let _ = provider.shutdown();
 
   Ok(())
 }
